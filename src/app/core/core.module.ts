9964ca808b9b6a315eaf1772e48a1ede27398852
Joe Ipe <joe.ipe@hotmail.com>
2024-06-05 13:55:17 +1000

    interceptors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptorFunctional, loggingInterceptorFunctional, responseTimeInterceptorFunctional } from './interceptor/functional.interceptor';
 
 
 
@@ -7,6 +9,18 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    provideHttpClient(
+      withInterceptors([
+        responseTimeInterceptorFunctional,
+        // loadingSpinnerInterceptorFunctional,
+        // authInterceptorFunctional,
+        // retryInterceptorFunctional,
+        loggingInterceptorFunctional,
+        errorInterceptorFunctional
+      ])
+    )
   ]
 })
 export class CoreModule { }
