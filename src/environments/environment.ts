8210b07408e7f75ee7a3707ad353982d5e525687
Joe Ipe <joe.ipe@hotmail.com>
2024-06-21 13:07:02 +1000

    update domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     azureAD: {
         authority: 'https://login.microsoftonline.com/031162b7-774f-40b2-a8a5-8b979557e49a/v2.0',
         clientId: '8453dda4-8e94-4928-8655-9a29018d2eea',
-        redirectUri: 'https://infosafe.azurewebsites.net/',
+        redirectUri: 'https://theinfosafe.com/',
         infoSafeApiScope: 'api://6bc1a37b-da3e-4b89-b621-c75c8eebee0a/FullAccess'
     },
     stripe: {
